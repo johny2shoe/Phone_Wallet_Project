de124132b30af11708f144458af482a9b6841c6f
johny2shoe <32992561+johny2shoe@users.noreply.github.com>
2020-05-12 23:55:53 -0400

    Add files via upload

---
+++ phone/README.md
@@ -0,0 +1,1 @@
+# Phone_Wallet_Project
---
+++ phone/main.js
@@ -0,0 +1,36 @@
+function app1Click() {
+  var x = document.getElementById("app1-display");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function app2Click() {
+  var x = document.getElementById("app-display");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function app3Click() {
+  var x = document.getElementById("app3-display");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function app4Click() {
+  var x = document.getElementById("app4-display");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
